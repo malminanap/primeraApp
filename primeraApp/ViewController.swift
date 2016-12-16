c81059c89837e358c73dea306381c46b4ee91d84
Mario Pablo <mario@MacBook-Pro.local>
2016-12-16 18:07:06 +0100

    Initial Commit

---
+++ primeraApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  primeraApp
+//
+//  Created by Mario Almiñana Pablo on 16/12/16.
+//  Copyright © 2016 Mario Almiñana Pablo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
